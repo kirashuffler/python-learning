19e5d744467d99221dc5d102e7e0996f411d0f4d
Azamat Abzalov <kirashuffler@gmail.com>
2022-04-06 13:41:28 +0300

    fixed some review remarks, added profiling

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import time
+import yappi
 from http.client import HTTPSConnection
 from html.parser import HTMLParser
 from urllib.parse import urlparse
 from urllib.parse import urljoin
+
+number_of_requests = 0
 
 
 class CustomHTMLParser(HTMLParser):
@@ -13,38 +15,44 @@ class CustomHTMLParser(HTMLParser):
         super().__init__()
 
     def handle_data(self, data: str) -> None:
-        if '404' in data:
-            return
         self.word_count += data.count(self.search_word)
 
     def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]) -> None:
-        if tag == 'a' and 'href' == attrs[0][0] and '.html' in attrs[0][1]:
+        if len(attrs) > 0 and 'href' == attrs[0][0] and '.html' in attrs[0][1]:
             self.links_set.add(attrs[0][1])
 
 
-def generate_html_data(url: str) -> str:
+def fetch_html_data(url: str) -> str:
     parsed_url = urlparse(url)
     if parsed_url.fragment != '':
         return 'none'
     connection = HTTPSConnection(parsed_url.hostname)
     connection.request("GET", parsed_url.path)
     connection_response = connection.getresponse()
+    if connection_response.status != 200:
+        return 'none'
     connection_data = connection_response.read()
     return connection_data.decode()
 
 
+def generate_absolute_links(host: str, relative_links: set[str]) -> set[str]:
+    absolute_links = set()
+    for relative_link in relative_links:
+        absolute_links.add(urljoin(host, relative_link))
+    return absolute_links
+
+
 def count_word_on_page_and_subpages(url: str, word: str, visited_links=set()) -> int:
     count = 0
-    html_data = generate_html_data(url)
+    html_data = fetch_html_data(url)
     if html_data == 'none':
         return count
     parser = CustomHTMLParser(word)
     parser.feed(html_data)
     count += parser.word_count
-    sublinks = set()
-    for relative_link in parser.links_set:
-        sublinks.add(urljoin(main_url, relative_link))
+    sublinks = generate_absolute_links(url, parser.links_set)
     visited_links.add(url)
+    print(url)
     if len(sublinks) == 0:
         return count
     for link in sublinks:
@@ -54,10 +62,19 @@ def count_word_on_page_and_subpages(url: str, word: str, visited_links=set()) ->
     return count
 
 
-if __name__ == '__main__':
+def main():
     main_url = 'https://docs.python.org/3/library/urllib.parse.html'
-    start = time.time()
     n = count_word_on_page_and_subpages(main_url, 'optional')
-    end = time.time()
-    print("Elapsed time in seconds:", end - start)
     print(n)
+    print("Number of request: ", number_of_requests)
+
+
+if __name__ == '__main__':
+    yappi.start()
+    main()
+    func_stats = yappi.get_func_stats()
+    func_stats.print_all()
+    func_stats.save('yappi_output.pstats', 'PSTAT')
+    func_stats.save('yappi_output.callgrid','CALLGRIND')
+    yappi.stop()
+    yappi.clear_stats()
